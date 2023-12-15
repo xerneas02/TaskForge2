2246aee8d055611be7a2c23dcafa38121c464ebf
xerneas02 <mathis.roubille@gmail.com>
2023-12-15 14:27:09 +0100

    Pokemon is back

---
+++ MicroService/Pokemon/Controllers/PokemonController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pokemon.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pokemon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class PokemonController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PokemonController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Entities.Pokemon>>> GetPokemons()
+        {
+            return Ok(await _context.Pokemons.ToListAsync());
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Entities.Pokemon>> GetPokemon(int id)
+        {
+             return Ok(await _context.Pokemons.FirstOrDefaultAsync(user => user.Id == id));
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<ActionResult<Entities.Pokemon>> PostPokemon(Entities.Pokemon pokemon)
+        {
+            // On ajoute notre utilisateur dans la base
+            _context.Pokemons.Add(pokemon);
+            // On sauvegarde la modification
+            await _context.SaveChangesAsync();
+            // On retourne l'utilisateur nouvellement crée en appelant la fonction CreatedAtAction
+            return CreatedAtAction(nameof(GetPokemon), new { id = pokemon.Id }, pokemon);
+        }
+    }
+}
---
+++ MicroService/Pokemon/Migrations/20231208152045_Init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pokemon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nature = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ability = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
