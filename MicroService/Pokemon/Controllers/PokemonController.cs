cc484805464955d909b04a67b1aea112aa984e82
xerneas02 <mathis.roubille@gmail.com>
2024-01-04 17:06:50 +0100

    ajout d'un pokemon avec un id

--- MicroService/Pokemon/Controllers/PokemonController.cs
+++ MicroService/Pokemon/Controllers/PokemonController.cs
@@ -129,5 +129,75 @@ namespace pokemon.Controllers
                 throw new Exception($"An error occurred while getting a random Pokemon template: {ex.Message}");
             }
         }
+
+        [HttpPost("AddPokemonFromTemplate")]
+        public async Task<ActionResult<Entities.Pokemon>> AddPokemonFromTemplate(int trainerId, int templateId)
+        {
+            try
+            {
+                // Retrieve the Pokemon template with the specified templateId from the PokemonTemplate microservice
+                Entities.Pokemon randomTemplate = GetPokemonTemplateById(templateId);
+
+                // Create a new Pokemon using the retrieved template and provided trainer ID
+                Entities.Pokemon newPokemon = new Entities.Pokemon(
+                    trainerId,
+                    randomTemplate.Nom,
+                    randomTemplate.Type,
+                    false // Set the Shiny property as needed
+                );
+
+                // Add the new Pokemon to the Pokemon microservice database
+                _context.Pokemons.Add(newPokemon);
+                await _context.SaveChangesAsync();
+
+                // Return the newly created Pokemon
+                return CreatedAtAction(nameof(GetPokemon), new { id = newPokemon.Id }, newPokemon);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur during the process
+                return BadRequest($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private Entities.Pokemon GetPokemonTemplateById(int templateId)
+        {
+            try
+            {
+                // Make an HTTP request to the PokemonTemplate microservice endpoint with the specified templateId
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    string pokemonTemplateApiUrl = $"http://localhost:5227/api/pokemonTemplate/{templateId}";
+                    HttpResponseMessage response = httpClient.GetAsync(pokemonTemplateApiUrl).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResult = response.Content.ReadAsStringAsync().Result;
+
+                        var templateData = new { Nom = "", Type = Entities.PokemonType.Normal };
+                        var pokemonTemplate = JsonConvert.DeserializeAnonymousType(jsonResult, templateData);
+
+                        // Create a new Pokemon using the retrieved template
+                        Entities.Pokemon templatePokemon = new Entities.Pokemon(
+                            -1,
+                            pokemonTemplate.Nom,
+                            pokemonTemplate.Type,
+                            false // Set the Shiny property as needed
+                        );
+
+                        return templatePokemon;
+                    }
+                    else
+                    {
+                        throw new Exception($"Failed to retrieve Pokemon template with ID {templateId}. Status code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur during the process
+                throw new Exception($"An error occurred while getting Pokemon template with ID {templateId}: {ex.Message}");
+            }
+        }
     }
 }
