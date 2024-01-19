bb1abd6bc4c3b9ba7b216e4cbece4e3a56f35850
Silver-Ink <martin.74@orange.fr>
2024-01-19 15:18:09 +0100

    creation du jwt dans gateway

--- MicroService/GatewayService/Controllers/UserController.cs
+++ MicroService/GatewayService/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 using UserService.Entities;
 
 namespace GatewayService.Controllers
@@ -16,6 +20,30 @@ namespace GatewayService.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
+
+    private string GenerateJwtToken(int userId)
+    {
+        var claims = new List<Claim>
+        {
+            // On ajoute un champ UserId dans notre token avec comme valeur userId en string
+            new Claim("UserId", userId.ToString()) 
+        };
+
+        // On créer la clé de chiffrement
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSecretKeyLongLongLongLongEnough"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        // On paramètre notre token
+        var token = new JwtSecurityToken(
+            issuer: "TodoProject", // Qui a émit le token
+            audience: "localhost:5000", // A qui est destiné ce token
+            claims: claims, // Les données que l'on veux encoder dans le token
+            expires: DateTime.Now.AddMinutes(3000), // Durée de validité
+            signingCredentials: creds); // La clé de chiffrement
+
+        // On renvoie le token signé
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
 
         // api/User/login
         [HttpPost("login")]
